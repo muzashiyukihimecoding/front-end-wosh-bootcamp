e5520c13b9933976c91437a549da257519c7a5bc
Rasyid Nabil Pujantara <rasyidnabil06@gmail.com>
2024-12-07 11:49:04 +0700

    add new js dom 7 december 2024
    we're still learing bruh... :)

---
+++ js/domscript.js
@@ -0,0 +1,8 @@
+// Main Catalogue Image Changer Function
+const imgCatalogue = document.querySelector(".main-catalogue");
+function imgCatChange1() {
+   imgCatalogue.style.backgroundImage = "url(assets/brown-kicks-2.png)";
+}
+function imgCatChange2() {
+   imgCatalogue.style.backgroundImage = "url(assets/brown-kicks-3.png)";
+}
